8bcf8bb3652b082ebf9f56e4abb192ae2427b5bd
DustTail <dusttailtale@gmail.com>
2023-01-06 23:05:00 +0200

    Login page
    ### features
    - Added login page layout

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,9 +1,17 @@
-import { Box, Container } from "@mui/material";
+import { Box } from "@mui/material";
+import style from "./styles.module.scss";
 
 export default function Login() {
   return (
-    <Container>
-      <Box>Login</Box>
-    </Container>
+    <Box
+      sx={{
+        width: "100vw",
+        height: "100vh",
+        margin: "0",
+      }}
+      className={style.screen}
+    >
+      <div className={style.title}>Щопонеділка</div>
+    </Box>
   );
 }
